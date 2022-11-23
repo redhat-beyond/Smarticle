77494c0919ecb7ff5493139a518031e888ae5909
Shalevro2 <shalevro2@gmail.com>
2022-11-23 18:39:11 +0200

    Fixed flake8 test

--- projboard/migrations/0003_alter_article_subject_id.py
+++ projboard/migrations/0003_alter_article_subject_id.py
@@ -14,6 +14,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='article',
             name='subject_id',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='projboard.subject'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL,
+                                    to='projboard.subject'),
         ),
     ]
