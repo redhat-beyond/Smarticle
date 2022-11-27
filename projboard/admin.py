2cbe852c7e30e3336ad0105db7c0749723dc122d
Anas-Mhameed <mhameedanaskhader@Gmail.com>
2022-11-27 14:43:47 +0200

    Adding models to the Django admine UI

--- projboard/admin.py
+++ projboard/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Article,User,Like,View 
 
 # Register your models here.
+
+admin.site.register(Article)
+admin.site.register(User)
+admin.site.register(Like)
+admin.site.register(View)
