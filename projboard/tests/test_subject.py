dc730b01dac32c487d2f79b45219b24c978b1d25
yael noyman <yael.nueman@gmail.com>
2022-11-30 10:46:07 +0200

    Fix models.py and test_subject.py
    in models.py i changed the name of the function "create_new_subject" to "creare_and_get_subject"
    and in test_subject.py i added a fixture and i changed the functions.

--- projboard/tests/test_subject.py
+++ projboard/tests/test_subject.py
@@ -1,52 +1,45 @@
 import pytest
 from projboard.models import Subject
 
-SPORT = "sport"
 ANIMALS = "Animals"
 NOTEXIST = "notexist"
 REDHAT = "redhat"
+NAME = "yael"
 
 
 @pytest.fixture
-def generate_subject(name='name'):
-    subject = Subject(name=name)
-    subject.save()
-    return subject
+@pytest.mark.django_db
+def generate_subject():
+    subject1 = Subject.create_and_get_subject(name="yael")
+    subject2 = Subject.create_and_get_subject(name="gal")
+    subject1.save()
+    subject2.save()
+    return [subject1, subject2]
 
 
+@pytest.mark.django_db
 class TestSubjectModel:
-    @pytest.mark.django_db
-    def test_create_subject(db):
-        subject = Subject.create_new_subject(SPORT)
-        assert subject.name == SPORT
+    def test_create_subject(self, generate_subject):
+        subject = generate_subject[0]
+        assert subject.name == NAME
 
-        subject = Subject.create_new_subject(SPORT)
-        assert subject.name == SPORT
+    def test_delete_subject(self, generate_subject):
+        assert Subject.delete_subject(generate_subject[0])
+        subject = Subject.get_subject_by_name(generate_subject[0])
+        assert not subject
 
-    @pytest.mark.django_db
     def test_update_subject(self, generate_subject):
-        Subject.create_new_subject(generate_subject)
-        subject = Subject.edit_subject(generate_subject, ANIMALS)
+        subject = generate_subject[0]
+        subject = Subject.edit_subject(generate_subject[0], ANIMALS)
         assert subject.name == ANIMALS
 
         subject = Subject.edit_subject(NOTEXIST, ANIMALS)
         assert not subject
 
     @pytest.mark.django_db
-    def test_delete_subject(self, generate_subject):
-        result = Subject.delete_subject(NOTEXIST)
-        assert not result
-
-        subject = Subject.create_new_subject(generate_subject)
-        result = Subject.delete_subject(subject.name)
-        assert result
-
-    @pytest.mark.django_db
     def test_get_list_subjects_names(self, generate_subject):
-        subject = Subject.create_new_subject(generate_subject)
-        subject1 = Subject.create_new_subject(ANIMALS)
-        subject2 = Subject.create_new_subject(REDHAT)
+        subject = generate_subject[0]
+        subject1 = generate_subject[1]
         listSubject = Subject.get_list_subjects_names()
         assert subject.name in listSubject
         assert subject1.name in listSubject
-        assert subject2.name in listSubject
