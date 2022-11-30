033bfc9d62066b95448471aa9a9d1797d788b9ec
Shalevro2 <shalevro2@gmail.com>
2022-11-30 18:43:45 +0200

    Changing in fixtures
    Changing the fixtures in test_like.py
    Adding more tests

--- projboard/tests/test_like.py
+++ projboard/tests/test_like.py
@@ -4,6 +4,7 @@ from projboard.models import Like, User, Article, Subject
 
 TITLE = "Article Test"
 CONTENT = "I'm not gonna write all that!"
+SUBJECT = "TEST"
 NAME = "Shalev"
 
 
@@ -12,51 +13,44 @@ class TestLikeModel:
 
     @pytest.fixture
     @pytest.mark.django_db
-    def create_subjects(self):
-        sup = Subject(name='N')
-        sup1 = Subject(name='R')
-        sup.save()
-        sup1.save()
-        return [sup, sup1]
+    def create_subject(self):
+        subject = Subject(name=SUBJECT)
+        subject.save()
+        return subject
 
     @pytest.fixture
     @pytest.mark.django_db
     def create_user(self):
-        user = User(name=NAME, nickname="S99", email="Shalev@gmail.com", password="Shalev123")
-        user1 = User(name="John", nickname="J99", email="John@gmail.com", password="John123")
+        user = User(name=NAME, nickname=f"{NAME}os", email=f"{NAME}@mail.com", password="f{NAME}123")
         user.save()
-        user1.save()
-        return [user, user1]
+        return user
 
     @pytest.fixture
     @pytest.mark.django_db
-    def create_articles(self, create_user, create_subjects):
-        articles_list = []
-        for i in range(len(create_user)):
-            article = Article(user_id=create_user[i], title=TITLE,
-                              content=CONTENT, subject_id=create_subjects[i])
-            article.save()
-            articles_list.append(article)
-
-        return articles_list
+    def create_article(self, create_user, create_subject):
+        article = Article(user_id=create_user, title=TITLE, content=CONTENT, subject_id=create_subject)
+        article.save()
+        return article
 
     @pytest.fixture
     @pytest.mark.django_db
-    def create_like(self, create_user, create_articles):
-        like = Like(user_id=create_user[0], article_id=create_articles[0])
+    def create_like(self, create_user, create_article):
+        like = Like(user_id=create_user, article_id=create_article)
         like.save()
         return like
 
     def test_create_like(self, create_like):
-        assert create_like.user_id.name == NAME
+        assert create_like in Like.objects.all()
         assert create_like.article_id.title == TITLE
+        assert create_like.user_id.name == NAME
 
     def test_delete_like(self, create_user, create_like):
-        result = Like.delete_like(create_user[1], create_like.article_id)
-        assert not result
-
         result = Like.delete_like(create_like.user_id, create_like.article_id)
-        assert result
+        assert result  # check if method delete_like success to delete the object
+
+        assert create_like not in Like.objects.all()  # check that the object is not in the db
+        result = Like.delete_like(create_user, create_like.article_id)
+        assert not result  # check if method delete_like failed to delete the object
 
     @pytest.mark.django_db
     def test_amount_of_likes_article(self, create_like):
