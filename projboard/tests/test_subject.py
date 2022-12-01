27cc28c3a57cf4807f82fc599cc7a30d7efdcb13
yael noyman <yael.nueman@gmail.com>
2022-12-01 14:47:01 +0200

    fix instead  of one fixtures, to two fixtures

--- projboard/tests/test_subject.py
+++ projboard/tests/test_subject.py
@@ -10,31 +10,37 @@ NAME = "yael"
 @pytest.fixture
 @pytest.mark.django_db
 def generate_subject():
-    subject1 = Subject.create_and_get_subject(name="yael")
-    subject2 = Subject.create_and_get_subject(name="gal")
-    subject1.save()
-    subject2.save()
-    return [subject1, subject2]
+    subject = Subject.create_and_get_subject(name="yael")
+    subject.save()
+    return subject
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def generate_subjects(generate_subject):
+    subject = Subject.create_and_get_subject(name="gal")
+    subject.save()
+    return [subject, generate_subject]
 
 
 @pytest.mark.django_db
 class TestSubjectModel:
     def test_create_subject(self, generate_subject):
-        assert generate_subject[0] in Subject.objects.all()
+        assert generate_subject in Subject.objects.all()
 
     def test_delete_subject(self, generate_subject):
-        Subject.delete_subject(generate_subject[0])
-        assert generate_subject[0] not in Subject.objects.all()
+        Subject.delete_subject(generate_subject)
+        assert generate_subject not in Subject.objects.all()
 
     def test_update_subject(self, generate_subject):
-        subject = Subject.rename_subject(generate_subject[0], ANIMALS)
+        subject = Subject.rename_subject(generate_subject, ANIMALS)
         assert subject.name == ANIMALS
 
         subject = Subject.rename_subject(NOTEXIST, ANIMALS)
         assert not subject
 
     @pytest.mark.django_db
-    def test_get_list_subjects_names(self, generate_subject):
+    def test_get_list_subjects_names(self, generate_subjects):
         listSubject = Subject.get_list_subjects_names()
-        for i in generate_subject:
+        for i in generate_subjects:
             assert i.name in listSubject
