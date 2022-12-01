8f8e479aba55ae29092ec8a6c45f735a7ca87dc5
Rawad Shakeb <rawadabusaleh0@gmail.com>
2022-12-01 10:11:15 +0200

    Updating test_user.py

--- projboard/tests/test_user.py
+++ projboard/tests/test_user.py
@@ -3,38 +3,45 @@ from projboard.models.user import User
 
 
 NAME = "RAWAD"
-NICKNAME = "R1998"
+NICKNAME = "USER3"
 EMAIL = "rawad@gmail.com"
-PASSWORD = "rawad!@#"
+PASSWORD = "123456"
 
-
-@pytest.fixture
-def generate_user():
-    user = User(name=NAME, nickname=NICKNAME, email=EMAIL, password=PASSWORD)
-    user.save()
-    return user
+# NAME1 = "RAWAD2"
+# NICKNAME1 = "USER32"
+# EMAIL1 = "rawad2@gmail.com"
+# PASSWORD1 = "1234562"
 
 
 class TestUserModel:
-    @pytest.mark.django_db()
+
+    @pytest.fixture
+    @pytest.mark.django_db
+    def generate_user(self):
+        user = User(email=EMAIL, password=PASSWORD, name=NAME, nickname=NICKNAME)
+        user.save()
+        return user
+
+    @pytest.mark.django_db
     def test_new_user(self, generate_user):
-        user = User.get_user(generate_user.email, generate_user.nickname, generate_user.name, generate_user.nickname)
+        user = User.get_user_by_nickname(generate_user.nickname)
+        assert generate_user.email == user.email
         assert generate_user.name == user.name
         assert generate_user.nickname == user.nickname
-        assert generate_user.email == user.email
         assert generate_user.password == user.password
 
-    @pytest.mark.django_db()
+    @pytest.mark.django_db
     def test_create_user(self, generate_user):
         assert generate_user in User.objects.all()
 
-    @pytest.mark.django_db()
+    # @pytest.mark.skip
+    @pytest.mark.django_db
     def test_delete_user(self, generate_user):
         # Delete user by nickname
         generate_user.delete_user(generate_user.nickname)
         assert generate_user not in User.objects.all()
 
-    @pytest.mark.django_db()
+    @pytest.mark.django_db
     def test_get_user(self, generate_user):
         # Get user by nickname
         assert User.get_user_by_nickname(generate_user.nickname) in User.objects.all()
